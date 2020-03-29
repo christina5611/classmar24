4c20be888993735b44154f74fadfdc9d797fb2e7
Toni <purple17peu@gmail.com>
2020-03-29 13:45:47 -0700

    k

--- Source.cpp
+++ Source.cpp
@@ -4,12 +4,12 @@ using namespace std;
 //swap
 
 void goodSwap(int& x, int& y, int& z);
-
+void badSwap(int& x, int& y, int& z);
 int main()
 
 {
 	// references
-	int bankBalance = 300050;
+	int bankBalance = 900050;
 	int hackedBalance = 605000;
 	int langleyBalance = 2;
 	cout << "\n\n\t\t***Check balances***\n\n";
@@ -18,15 +18,25 @@ int main()
 	cout << "The terrorist balance is : " << hackedBalance << "\n\n";
 	
 	cout << "The hacked balance is....\n\n";
-	goodSwap(bankBalance, hackedBalance);
+	goodSwap(bankBalance, hackedBalance, langleyBalance);
 	cout << "The bank balance is: " << bankBalance << "\n";
 	cout << "The terrorist balance is : " << hackedBalance << "\n\n";
-
+	badSwap(bankBalance, hackedBalance, langleyBalance);
+	cout << "The bank balance is: " << bankBalance << "\n";
+	cout << "The terrorist balance is : " << langleyBalance << "\n\n";
 	cout << "\n\n\t\t***Goodbye***\n\n";
 	return 0;
 }
 
 void goodSwap(int& bankBalance, int& hackedBalance, int& langleyBalance)
+{
+	int temp = bankBalance;
+	bankBalance = hackedBalance;
+	hackedBalance = temp;
+
+
+}
+void badSwap(int& bankBalance, int& hackedBalance, int& langleyBalance)
 {
 	int temp = bankBalance;
 	bankBalance = hackedBalance;
