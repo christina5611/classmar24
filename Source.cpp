b248bc60bce87b87ae7f06fa7f5f0e77a809b8a7
Toni <purple17peu@gmail.com>
2020-03-28 19:37:59 -0700

    ll

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 #include <string>
 using namespace std;
+//swap
+
+void goodSwap(int& x, int& y, int& z);
 
 int main()
 
 {
-	cout << "\n\n\t\t***The Refs***\n\n ";
-	//before you can make a reference you need to make a variable to refer to
-	int age = 21;
-	cout << "\nThe customer age using a variable is " << age << endl;
-
-	// make your first ref
-	int& r_age = age;
-	cout << "\nThe customer age using a reference is " << r_age << endl;
-	r_age = 25;
-	cout << "\nThe customer age using a reference is " << r_age << endl;
-	age = 16;
+	// references
+	int bankBalance = 300050;
+	int hackedBalance = 605000;
+	int langleyBalance = 2;
+	cout << "\n\n\t\t***Check balances***\n\n";
+	cout << "After the first transaction the balance for both accounts are....\n";
+	cout << "The bank balance is: " << bankBalance << "\n";
+	cout << "The terrorist balance is : " << hackedBalance << "\n\n";
 	
-	cout << "\nThe customer age using a variable is " << age << endl; 
-	cout << "\nThe customer age using a variable is " << age << endl;
+	cout << "The hacked balance is....\n\n";
+	goodSwap(bankBalance, hackedBalance);
+	cout << "The bank balance is: " << bankBalance << "\n";
+	cout << "The terrorist balance is : " << hackedBalance << "\n\n";
 
+	cout << "\n\n\t\t***Goodbye***\n\n";
 	return 0;
+}
+
+void goodSwap(int& bankBalance, int& hackedBalance, int& langleyBalance)
+{
+	int temp = bankBalance;
+	bankBalance = hackedBalance;
+	hackedBalance = temp;
+	hackedBalance = langleyBalance;
+
 }
